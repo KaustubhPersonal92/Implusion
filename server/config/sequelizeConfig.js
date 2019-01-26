0d6740a62afb5568374fa57edb1ae7c802fba75d
Kaustubh Mishra <kaustubh92@outlook.com>
2019-01-26 22:14:55 +0530

    Db Changes

--- server/config/sequelizeConfig.js
+++ server/config/sequelizeConfig.js
@@ -4,22 +4,22 @@ module.exports = {
     modelsDir : {
         path : __dirname + '/../models'
     },
-    // "live-db": {
-    //     "username": 'admin',
-    //     "password": 'admin123',
-    //     "database": 'impulsion',
-    //     "host": '34.220.168.152',
+    "db": {
+        "username": 'admin',
+        "password": 'admin123',
+        "database": 'impulsion',
+        "host": '34.220.168.152',
+        "dialect": 'mysql',
+        "port": '3306'
+    },
+
+    // "db": {
+    //     "username": 'root',
+    //     "password": '12345678',
+    //     "database": 'implusion',
+    //     "host": 'localhost',
     //     "dialect": 'mysql',
     //     "port": '3306'
     // }
-
-    "db": {
-        "username": 'root',
-        "password": '12345678',
-        "database": 'implusion',
-        "host": 'localhost',
-        "dialect": 'mysql',
-        "port": '3306'
-    }
 
 };
